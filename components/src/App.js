ce0120d8dcc7dfda64fe2dcd66c3f295673e3484
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-01-27 14:32:25 +0100

    adding new components

--- components/src/App.js
+++ components/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import Title from './components/Title';
-import Paragraph from './components/Paragraph';
+import Welcome from './components/Welcome';
 
 function App() {
+	let today = new Date(Date.now()).toLocaleDateString();
 	return (
 		<div className='main'>
-			<Title />
-			<Paragraph />
-			<Paragraph />
+			<Welcome
+				userName='Manuel'
+				today={today}
+			/>
+			<Welcome
+				userName='Elena'
+				today={today}
+			/>
+			<Welcome
+				userName='Alexis'
+				today={today}
+			/>
 		</div>
 	);
 }
