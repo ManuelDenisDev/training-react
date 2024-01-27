9c7966db7fba7829bd10df24895981794d02cbcf
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-01-27 02:06:26 +0100

    cleanTemplate

---
+++ clean-template/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+			</p>
+		</div>
+	);
+}
+
+export default App;
