bfd4adf23aefddbae8174929595caf10a39c4dd8
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-01-27 11:00:32 +0100

    clean template ready

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<h1>Hello, World! Clena Template</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
 				Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet
