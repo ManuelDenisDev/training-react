b8a51a83d97f8ad45b9a8ff6c466573ae03fda61
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-01-29 18:17:28 +0100

    css ready

--- cards/src/App.js
+++ cards/src/App.js
@@ -13,51 +13,74 @@ import { faStar, faCircleCheck, faDollarSign, faEnvelope, faAddressBook, faPhone
 
 // Componente principal
 function App() {
+	// Datos de prueba
+	const freelancer = [
+		{
+			id: 0,
+			firstName: 'MR',
+			lastName: 'ROBERT',
+			role: 'UI/UX Designer',
+			rate: 85,
+			rating: 5,
+			reviews: 105,
+		},
+	];
+
 	return (
-		<CardWrapper>
-			<div className='img-container'>
-				<div className='img-wrapper'>
-					<img
-						src={freelancer1}
-						alt='freelancer-one'
-					/>
-					<span className='dot'></span>
-				</div>
-			</div>
-			<div className='info-container'>
-				<h3>MR ROBERT</h3>
-				<p>UI/UX Designer</p>
-				<div className='hire'>
-					<div>
-						Rate: <span>$85/hr</span>
+		<div className='card-container'>
+			<CardWrapper>
+				<div className='img-container'>
+					<div className='img-wrapper'>
+						<img
+							src={freelancer1}
+							alt='freelancer-one'
+						/>
+						<span className='dot'></span>
 					</div>
-					<button className='hire-button'>Hire ME</button>
 				</div>
-				<div className='rating'>
-					<div>5.0</div>
-					<div>
-						<FontAwesomeIcon icon={faStar} />
-						<FontAwesomeIcon icon={faStar} />
-						<FontAwesomeIcon icon={faStar} />
-						<FontAwesomeIcon icon={faStar} />
-						<FontAwesomeIcon icon={faStar} />
+				<div className='info-container'>
+					<h3>MR ROBERT</h3>
+					<p>UI/UX Designer</p>
+					<div className='hire'>
+						<div>
+							Rate: <span className='price'>$85/hr</span>
+						</div>
+						<button className='hire-button'>Hire ME</button>
 					</div>
-					<div>(105 Reviews)</div>
+					<div className='rating'>
+						<div>5.0</div>
+						<div>
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+						</div>
+						<div>(105 Reviews)</div>
+					</div>
 				</div>
-			</div>
-			<div className='icons-container'>
-				<div className='verified'>
-					<FontAwesomeIcon icon={faCircleCheck} />
-					<span>Verified</span>
+				<div className='icons-container'>
+					<div className='verified'>
+						<FontAwesomeIcon icon={faCircleCheck} />
+						<span>Verified</span>
+					</div>
+					<div className='icons-circle'>
+						<span className='circle available'>
+							<FontAwesomeIcon icon={faDollarSign} />
+						</span>
+						<span className='circle available'>
+							<FontAwesomeIcon icon={faEnvelope} />
+						</span>
+						<span className='circle not-available'>
+							<FontAwesomeIcon icon={faAddressBook} />
+						</span>
+						<span className='circle not-available'>
+							<FontAwesomeIcon icon={faPhoneFlip} />
+						</span>
+					</div>
 				</div>
-				<div>
-					<FontAwesomeIcon icon={faDollarSign} />
-					<FontAwesomeIcon icon={faEnvelope} />
-					<FontAwesomeIcon icon={faAddressBook} />
-					<FontAwesomeIcon icon={faPhoneFlip} />
-				</div>
-			</div>
-		</CardWrapper>
+			</CardWrapper>
+		</div>
 	);
 }
 
