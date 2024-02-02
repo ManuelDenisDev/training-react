43dd1055ad12da6e88d0bd9f5080e972f6e373ec
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-02-02 14:34:30 +0100

    styled componets ready

--- styles/src/App.jsx
+++ styles/src/App.jsx
@@ -1,13 +1,28 @@
 // Importing styled-components
 import styled from 'styled-components';
 
-const Wrapper = styled.section`
-	font-size: 1.5em;
+const Wrapper = styled.section.attrs((props) => ({
+	id: 'header',
+	size: props.size || '1.5em',
+}))`
+	font-size: ${(props) => props.size};
 	padding: 4em;
-	background: DarkOliveGreen;
+	background: ${(props) => (props.darkTheme ? 'DarkSlateGrey' : 'DarkOliveGreen')};
 	text-align: center;
-`;
 
+	&:hover h1 {
+		color: SkyBlue;
+	}
+	h1 {
+		font-size: 1.5em;
+		text-align: center;
+		color: AliceBlue;
+		margin-bottom: 1rem;
+		p {
+			color: YellowGreen;
+		}
+	}
+`;
 const Title = styled.h1`
 	font-size: 1.5em;
 	text-align: center;
@@ -15,15 +30,21 @@ const Title = styled.h1`
 	margin-bottom: 1rem;
 `;
 
+const Paragraph = styled(Title)`
+	color: YellowGreen;
+`;
+
 function App() {
 	return (
-		<Wrapper>
-			<Title>Hello, World!</Title>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-				quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing
-				elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-			</p>
+		<Wrapper darkTheme size={'1em'}>
+			<h1>
+				Hello, World!
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+				</p>
+			</h1>
 		</Wrapper>
 	);
 }
