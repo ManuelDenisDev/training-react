c544926c585675c38e31f545a1ba240aed86e9dd
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-02-02 11:37:40 +0100

    Empezando con Styled Componets

--- styles/src/App.jsx
+++ styles/src/App.jsx
@@ -1,15 +1,30 @@
-import './App.css';
+// Importing styled-components
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+	font-size: 1.5em;
+	padding: 4em;
+	background: DarkOliveGreen;
+	text-align: center;
+`;
+
+const Title = styled.h1`
+	font-size: 1.5em;
+	text-align: center;
+	color: AliceBlue;
+	margin-bottom: 1rem;
+`;
 
 function App() {
 	return (
-		<div>
-			<h1>Hello, World! Clena Template</h1>
+		<Wrapper>
+			<Title>Hello, World!</Title>
 			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet
-				consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+				quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing
+				elit. Quisquam, quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
 			</p>
-		</div>
+		</Wrapper>
 	);
 }
 
